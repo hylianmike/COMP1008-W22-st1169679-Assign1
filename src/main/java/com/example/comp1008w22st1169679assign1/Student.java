b77edbc06abd9ce50f3da099a0e112b5b87db794
Michael Rosanelli <michaelrosanelli105@gmail.com>
2022-02-13 11:10:18 -0500

    Initial commit

---
+++ src/main/java/com/example/comp1008w22st1169679assign1/Student.java
@@ -0,0 +1,83 @@
+package com.example.comp1008w22st1169679assign1;
+
+import javafx.scene.image.Image;
+
+public class Student {
+
+    private String firstName;
+    private String lastName;
+    private int studentNumber;
+    private Image image;
+
+    /**
+     * constructor for a student object
+     * @param firstName first name of the student
+     * @param lastName last name of the student
+     * @param studentNumber the student number
+     */
+
+    public Student(String firstName, String lastName, int studentNumber) {
+        setFirstName(firstName);
+        setLastName(lastName);
+        setStudentNumber(studentNumber);
+        String file = String.format("img/%s-%s.png", firstName.toLowerCase(), lastName.toLowerCase());
+        image = new Image(getClass().getResource(file).toExternalForm());
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    /**
+     * Validates to see if the parameter actually has content in it
+     */
+
+    public void setFirstName(String firstName) {
+        firstName = firstName.trim();
+        if (!firstName.equals(""))
+            this.firstName = firstName;
+        else
+            throw new IllegalArgumentException("You need to actually input a name");
+
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    /**
+     * Validates to see if the parameter actually has content in it
+     */
+
+    public void setLastName(String lastName) {
+        lastName = lastName.trim();
+        if (!lastName.equals(""))
+            this.lastName = lastName;
+        else
+            throw new IllegalArgumentException("You need to actually input a name");
+    }
+
+    public int getStudentNumber() {
+        return studentNumber;
+    }
+
+    /**
+     * Validates to make sure the number has 7 digits
+     */
+
+    public void setStudentNumber(int studentNumber) {
+        if (studentNumber >= 1000000 && studentNumber <= 9999999)
+            this.studentNumber = studentNumber;
+        else
+            throw new IllegalArgumentException("Input a 7 digit number");
+    }
+
+    public Image getImage() {
+        return image;
+    }
+
+    @Override
+    public String toString(){
+        return (firstName + " " + lastName + ": #" + studentNumber);
+    }
+}
---
+++ src/main/java/com/example/comp1008w22st1169679assign1/StudentCardViewController.java
@@ -0,0 +1,24 @@
+package com.example.comp1008w22st1169679assign1;
+
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.Label;
+import javafx.scene.image.ImageView;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+
+public class StudentCardViewController {
+
+    @FXML
+    private Label firstNameLabel;
+
+    @FXML
+    private Label lastNameLabel;
+
+    @FXML
+    private Label studentNumberLabel;
+
+    @FXML
+    private ImageView imageView;
+}
